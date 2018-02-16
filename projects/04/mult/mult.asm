974f7865ea3ac01d7a5e98c0338732a72c6b4a8f
Phil Geltman <pgeltman@gmail.com>
2018-02-15 21:11:01 -0800

    Got a mostly working piece of machine code to multiply

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,43 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//clear the result
+@R2
+M=0
+
+//clear the working result
+@R3
+M=0
+
+//jump to the end if r1 = 0
+@R1
+D=M
+@18 //last line of code + 1
+D;JGT
+
+//save input1 into d register
+@R0
+D=M
+
+//Save the result of d+m into the m
+@R3
+M=D+M
+
+//decrement input2 as countdown
+@R1
+M=M-1
+
+//Load R1 into d
+@R1
+D=M
+
+@8 //or the begging of the loop (line 24)
+D;JGT
+
+//set d to the working output
+@R3
+D = M
+
+//set the final output to d
+@R2
+M = D
